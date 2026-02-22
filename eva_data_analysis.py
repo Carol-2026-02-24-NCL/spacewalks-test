d2fac86a037731b03a48ec7f16ebae71e23cc5ad
Carol Booth <carol.booth2@newcastle.ac.uk>
2026-02-22 06:32:30 +0000

    Add duration by astronaut functionality. Fixes #2.

--- eva_data_analysis.py
+++ eva_data_analysis.py
@@ -4,7 +4,7 @@ import sys
 import re
 
 
-def main(input_file, output_file, graph_file):
+def main(input_file, output_file, duration_by_astronaut_output_file, graph_file):
     print("--START--")
 
     # Read the data from JSON file
@@ -21,6 +21,12 @@ def main(input_file, output_file, graph_file):
 
     # Plot cumulative time spent in space over years
     plot_cumulative_time_in_space(eva_data, graph_file)
+
+    # Calculate summary table for total EVA per astronaut
+    duration_by_astronaut_df = summary_duration_by_astronaut(eva_data)
+
+    # Save summary duration data by each astronaut to CSV file
+    write_dataframe_to_csv(duration_by_astronaut_df, duration_by_astronaut_output_file)
 
     print("--END--")
 
@@ -152,6 +158,24 @@ def add_crew_size_column(df):
     )
     return df_copy
 
+def summary_duration_by_astronaut(df):
+    """
+    Summarise the duration data by each astronaut and saves resulting table to a CSV file
+
+    Args: 
+        df (pd.DataFrame): Input dataframe to be summarised
+
+    
+    Returns:
+        sum_by_astro (pd.DataFrame): Data frame with a row for each astronaut and a summarised column 
+    """
+    print(f'Calculating summary of total EVA time by astronaut')
+    subset = df.loc[:,['crew', 'duration']] # subset to work with only relevant columns
+    subset = add_duration_hours(subset) # need duration_hours for easier calcs
+    subset = subset.drop('duration', axis=1) # dropping the extra 'duration' column as it contains string values not suitable for calulations
+    subset = subset.groupby('crew').sum() 
+    return subset
+
 
 if __name__ == "__main__":
 
@@ -165,5 +189,6 @@ if __name__ == "__main__":
         print('Using custom input and output filenames')
 
     graph_file = 'results/cumulative_eva_graph.png'
+    duration_by_astronaut_output_file = 'results/duration_by_astronaut.csv'
 
-    main(input_file, output_file, graph_file)
+    main(input_file, output_file, duration_by_astronaut_output_file, graph_file)
